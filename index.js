b68e754052dc8365843ab76305d3fdb4727c279f
tekab aymen <aymanktari31@gmail.com>
2022-08-15 11:33:08 +0100

    create server + connect to mongoDb

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
+const port = process.env.PORT || 5000;
+
+connectDB();
+//middleware;
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+app.listen(port, () => console.log(`Server started on port ${port}`));
